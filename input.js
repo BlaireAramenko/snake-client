34d1c7b364efd452ae82e68c5ba8b3166666dc4e
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-15 21:47:35 -0800

    edited

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,7 +18,17 @@ const setupInput = function () {
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === 'w') {
+    console.log('Move: up');
+  } else if (data === 'a') {
+    console.log('Move: left');
+  } else if (data === 's') {
+    console.log('Move: down');
+  } else if (data === 'd') {
+    connection.write('Move: right');
   }
 };
+
+
 
 module.exports = { setupInput };
